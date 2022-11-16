cb37d1d9ebadbe5e0a1afa6c6aec0313dc1b353f
Rafael <RafaFriedelDev@outlook.com>
2022-11-16 17:12:02 -0300

    fix: :bug: remove dead code

--- test-gethub/helpers/fetchs/getConfigurationApi.js
+++ test-gethub/helpers/fetchs/getConfigurationApi.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const myKey = process.env.NEXT_PUBLIC_MY_KEY;
 
-console.log(myKey);
-
 const getConfigurationApi = async () => {
   const url = `https://api.themoviedb.org/3/configuration?api_key=${myKey}`;
   const { data } = await axios.get(url);
