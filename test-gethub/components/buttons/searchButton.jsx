01dd9fc86ad558c0eeaad153de22ad2123dbe1ef
Rafael <RafaFriedelDev@outlook.com>
2022-11-16 15:34:45 -0300

    feat(components): :white_check_mark: create search input

--- test-gethub/components/buttons/searchButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SearchButton = () => {
-  return (
-    <button className="button is-dark is-mediun px-3">Buscar Filme</button>
-  );
-};
-
-export default SearchButton;
--- test-gethub/components/inputs/SearchInput.jsx
+++ test-gethub/components/inputs/SearchInput.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BsSearch } from 'react-icons/bs';
+import myContext from '../../context/myContext';
 
 const SearchInput = () => {
+  const { handleChange, state, setState } = useContext(myContext);
+
   return (
     <p className="control has-icons-left pr-3">
       <input
         className="input is-primary"
         type="text"
         placeholder="Qual o filme?"
+        name="movieName"
+        value={state.movieName}
+        onChange={(e) => handleChange(e, state, setState)}
       />
       <span className="icon is-left">
         <BsSearch />
