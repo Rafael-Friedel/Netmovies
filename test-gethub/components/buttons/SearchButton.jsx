24c78fbbfd9dd3c96e77e6db697ae017a62168d3
Rafael <RafaFriedelDev@outlook.com>
2022-11-17 14:51:33 -0300

    feat(components): :art: add business rule and stylize button

--- test-gethub/components/buttons/SearchButton.jsx
+++ test-gethub/components/buttons/SearchButton.jsx
@@ -6,12 +6,19 @@ const SearchButton = () => {
   const { state, setState } = useContext(myContext);
 
   const searchMovies = async () => {
+    setState({ ...state, loading: true });
     const listMovies = await getMoviesByName(state.movieName);
-    setState({ ...state, movieName: '', listMovies });
+    setState({ ...state, movieName: '', listMovies, loading: false });
   };
 
   return (
-    <button className="button is-dark is-mediun px-3" onClick={searchMovies}>
+    <button
+      className={`button is-dark is-mediun px-3 ${
+        state.loading && 'is-loading'
+      }`}
+      onClick={searchMovies}
+      disabled={state.movieName.length < 2}
+    >
       Buscar Filme
     </button>
   );
