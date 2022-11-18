183442bf71805bd4c8cb489ecb5cd914ff36c443
Rafael <RafaFriedelDev@outlook.com>
2022-11-18 16:22:30 -0300

    feat: :white_check_mark: add default image

---
+++ test-gethub/helpers/utils/defaultImg.js
@@ -0,0 +1,6 @@
+const defaultImg =
+  'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/' +
+  'glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93' +
+  '812ccb132b9df511df1cfac.svg';
+
+export default defaultImg;
