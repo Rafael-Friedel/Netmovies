40d643d00b2aada9cb3cc9e4afe3b95c641d591c
Rafael <RafaFriedelDev@outlook.com>
2022-11-19 17:47:56 -0300

    style: :lipstick: add logo in app

--- test-gethub/pages/movies/index.js
+++ test-gethub/pages/movies/index.js
@@ -4,6 +4,7 @@ import Search from '../../components/Search';
 import myContext from '../../context/myContext';
 import getConfigurationApi from '../../helpers/fetchs/getConfigurationApi';
 import defaultImg from '../../helpers/utils/defaultImg';
+import logo from '../../logo.png';
 
 export default function Movies() {
   const { state, setState } = useContext(myContext);
@@ -24,19 +25,33 @@ export default function Movies() {
         className="columns p-4 is-flex-wrap-wrap"
         style={{ background: '#191934' }}
       >
-        {state.listMovies.map(({ id, title, release_date, poster_path }) => (
-          <MovieCard
-            key={id}
-            name={title}
-            date={release_date.split('-').reverse().join('/')}
-            img={
-              poster_path
-                ? `${state.configuration.images.secure_base_url}original${poster_path}`
-                : defaultImg
-            }
-            id={id}
+        {state.listMovies.length > 0 ? (
+          state.listMovies.map(({ id, title, release_date, poster_path }) => (
+            <MovieCard
+              key={id}
+              name={title}
+              date={release_date.split('-').reverse().join('/')}
+              img={
+                poster_path
+                  ? `${state.configuration.images.secure_base_url}original${poster_path}`
+                  : defaultImg
+              }
+              id={id}
+            />
+          ))
+        ) : (
+          <img
+            src={logo.src}
+            alt="logo da página"
+            style={{
+              width: '100%',
+              height: '100%',
+              textAlign: 'center',
+              objectFit: 'cover',
+              margin: '178px',
+            }}
           />
-        ))}
+        )}
       </section>
     </main>
   );
