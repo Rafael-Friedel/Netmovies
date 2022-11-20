19a6897eb6fa024cf2e057835927026a3dfc31b8
Rafael <RafaFriedelDev@outlook.com>
2022-11-20 20:26:24 -0300

    refactor: :sparkles: add simple  search with enter

--- test-gethub/components/buttons/SearchButton.jsx
+++ test-gethub/components/buttons/SearchButton.jsx
@@ -3,20 +3,14 @@ import myContext from '../../context/myContext';
 import getMoviesByName from '../../helpers/fetchs/getMoviesByName';
 
 const SearchButton = () => {
-  const { state, setState } = useContext(myContext);
-
-  const searchMovies = async () => {
-    setState({ ...state, loading: true });
-    const listMovies = await getMoviesByName(state.movieName);
-    setState({ ...state, movieName: '', listMovies, loading: false });
-  };
+  const { state } = useContext(myContext);
 
   return (
     <button
       className={`button is-dark is-mediun px-3 ${
         state.loading && 'is-loading'
       }`}
-      onClick={searchMovies}
+      type="submit"
       disabled={state.movieName.length < 2}
     >
       Buscar Filme
